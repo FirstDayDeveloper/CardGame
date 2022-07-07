8f8a3dfa26a290a40b7b0ce3049351e07062492e
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-07-07 17:36:58 +1000

    Programmed the Deal Tapped button and wrote randomization code

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -35,7 +35,18 @@ class ViewController: UIViewController {
     
     @IBAction func dealTapped(_ sender: Any) {
         
-        print("Deal Tapped")
+        let leftNumber = Int.random(in: 2...14)
+        
+        //print(leftNumber)
+        
+        let rightNumber = Int.random(in: 2...14)
+        
+        //print(rightNumber)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
     }
     
 }
